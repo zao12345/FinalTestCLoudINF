640cea6aaa65361df1c5506d25d8f196e84edfd0
Zainab Abdussalam-Osimade <74374137+zao12345@users.noreply.github.com>
2024-04-12 11:16:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+import socket
+
 app = Flask(__name__)
- 
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
 @app.route('/')
 def hello_cloud():
-    return 'Hello from Osimade ECS Container'
- 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+  return 'Welcome to Osimade Final Test API Server!'
+  
+@app.route('/host')
+def host_name():
+  return hostname
+
+@app.route('/ip')
+def host_ip():
+  return ip_address
+
+app.run(host='0.0.0.0')
+
